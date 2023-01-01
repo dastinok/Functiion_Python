7b006e6c71fa1d230094b7bdfe52c25f029038c9
Denis <denyashin@yandex.ru>
2023-01-01 08:12:59 +0300

    Add file

---
+++ bad_local.py
@@ -0,0 +1,12 @@
+
+def main():
+    var1 = 'klara'
+    print(var1)
+    get_name(var1)
+
+
+def get_name(var2):
+    name = input('Yor name: ')
+    var2 = name
+    print(var2)
+main()
---
+++ Профессиональный техсервис.py
@@ -0,0 +1,37 @@
+#Эта программа показывает пошаговые инструкции
+#для разборки бельевой сушилки Acme
+# Главная ф-ция выполняет основную логику программы
+
+def main():
+    #Показать стартовое сообщение
+    startup_message()
+    input('Нажмите Enter, чтобы увидеть шаг 1')
+    step1()
+    input('Нажмите Enter, чтобы увидеть шаг 2')
+    step2()
+    input('Нажмите Enter, чтобы увидеть шаг 3')
+    step3()
+    input('Нажмите Enter, чтобы увидеть шаг 4')
+    step4()
+
+
+
+def startup_message():
+    pass
+
+def step1():
+    pass
+
+def step2():
+    pass
+
+def step3():
+    pass
+
+def step4():
+    pass
+
+
+
+
+main()
