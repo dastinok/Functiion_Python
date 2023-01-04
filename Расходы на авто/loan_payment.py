7cf8f74ecf2ae86253d709a1eb3813de607264fd
Denis <denyashin@yandex.ru>
2023-01-04 08:03:20 +0300

    Add file

--- Расходы на авто/loan_payment.py
+++ Расходы на авто/loan_payment.py
@@ -1,4 +1,5 @@
 def loan_payment():
     credit = float(input('Введите платеж по кредиту: '))
     print(f'Платеж по кредиту = {credit:,.2f} рублей')
+    print('******************************************')
     return credit
